e2d1cba574020ab23a78bcda03dffd30bf7b100e
srino <srinoytt@gmail.com>
2025-11-20 16:36:59 +0100

    feat: uploading

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,30 @@
 plugins {
     kotlin("jvm") version "2.2.20"
-    id("io.ktor.plugin") version "3.3.2"
+    `maven-publish`
 }
 
-group = "org.srino"
+group = "io.github.galsever"
 version = "0.0.1"
+description = "Ktor Upload to S3 Library"
 
 dependencies {
-    implementation("aws.sdk.kotlin:s3:1.0.50")
+    compileOnly("aws.sdk.kotlin:s3:1.0.50")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            groupId = "com.github.galsever"
+            artifactId = "ktor-upload"
+            version = "0.0.1"
+
+            pom {
+                name.set("ktor-upload")
+                description.set("Ktor Upload to S3 Library")
+                url.set("https://github.com/galsever/ktor-upload")
+            }
+        }
+    }
 }
